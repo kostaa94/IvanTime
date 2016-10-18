b7fabca0c1641799742c1cc12893eb0ad709b720
Novoselov Ivan <a1302626@myy.haaga-helia.fi>
2016-10-18 14:26:47 +0300

    Time working

---
+++ src/main/java/fi/haagahelia/time/util/HelloWorld.java
@@ -0,0 +1,18 @@
+package fi.haagahelia.time.util;
+
+
+/**
+ * Software to print on console
+ * @author a1302626, Ivan Novoselov
+ * @version 18.10.2016
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//print console
+		System.out.println("Hello, world!");
+
+	}
+
+}
